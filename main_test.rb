bb6bdb6ba9b627f8d145193cf534f871bdcd5aa5
Yuta Miyama <s04155yo@gmail.com>
2021-05-22 19:50:51 -0400

    Lay out the basic commands

--- main_test.rb
+++ main_test.rb
@@ -15,6 +15,23 @@ describe Main do
     ]
   end
 
+  it "simulates the single game play" do
+    # start with initial energy and money
+    # buy book
+    # buy cookie
+    #
+    # schedule book and cookie
+    # wait in game time
+    # assert that energy and skills change accordingly
+    #
+    # apply for a job
+    # assert that it can succeed with the combination of luck and skills
+    #
+    # schedule a job
+    # wait in game time
+    # assert money and energy and skills change accordingly
+  end
+
   it "terminates if terminating space attr goes zero" do
     @s2.add_condition(->(v) { v < 0 }, :game_ends)
     base = {
