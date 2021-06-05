31e8282fef69ef173a5b6a84a286dbd023d64a39
Yuta Miyama <s04155yo@gmail.com>
2021-06-05 15:04:48 -0400

    Make sure tests are passing again

--- test/main_test.rb
+++ test/main_test.rb
@@ -37,27 +37,19 @@ describe 'Game CLI mode - end to end' do
 
   it "simulates the single game play" do
     money_space = Constructs::Space.new(:money)
-    purchase_vec = Constructs::Vector.new(money_space, 10)
-
-    ev1 = Events::Event.new(:purchase, @cs_book, [purchase_vec])
-    ev2 = Events::Event.new(:purchase, @cookie,  [purchase_vec])
-
-    _(Aggregates::Inventory.new([ev1, ev2]).call.to_s).must_equal(
-      "cs_book * 1 | permanent\ncookie * 1 | consumable"
-    )
 
     ev3 = Events::Event.new(:schedule, @cs_book, [], { as: :study, from: 9, till: 10})
     ev4 = Events::Event.new(:schedule, @cookie,  [], { as: :eat,   from: 10, till: 12})
 
     _(Aggregates::Schedule.new([ev3, ev4]).call.to_s).must_equal(
       " 9 - 10 | study cs_book (cs_skill +10, energy -5)\n" +
-      "10 - 12 | eat cookie (energy +14)"
+      "10 - 12 | eat cookie (energy +28)"
     )
 
-    ev5 = Events::GameTime.new(:tick, :game_time, [])
-    ev6 = Events::GameTime.new(:tick, :game_time, [])
+    ev5 = Events::GameTime.new(:tick, :game_time, [], { speed_val: 1 })
+    ev6 = Events::GameTime.new(:tick, :game_time, [], { speed_val: 2 })
 
-    produced_events = Aggregates::TimeProgress.new([ev3, ev4, ev5, ev6]).call
+    produced_events = Aggregates::TimeProgress.new([ev3, ev4, ev5, ev6]).call.events
 
     _(produced_events.group_by(&:action)[:study].first.target).must_equal @cs_book
     _(produced_events.group_by(&:action)[:eat].first.target).must_equal @cookie
@@ -66,44 +58,32 @@ describe 'Game CLI mode - end to end' do
 
     _(result.to_h).must_equal(
       {
-        stats: { cs_skill: 10, energy: 9 },
-        inventory: ""
+        :stats=>{:money=>"100", :energy=>"28"},
+        :violations=>[]
       }
-    )
-
-    _(result.side_effects.first.action).must_equal :consume
-    _(result.side_effects.first.target).must_equal @cookie
-
-    _(Aggregates::Inventory.new([ev1, ev2] + result.side_effects).call.to_s).must_equal(
-      "cs_book * 1 | permanent\ncookie * 0 | consumable"
     )
 
     @software_engineer = Static::Allocatable.new(
       :software_engineer,
-      30,
-      { cs_skill: 10 },
-      { cs_skill: 3 },
-      { energy: -10 },
+      :job
     )
-    job_apply = Events::Event.new(:apply, @software_engineer, [])
-
-    fail = Aggregates::JobApplication.new([job_apply]).call
-    _(fail.bool).must_equal(false)
-
-    success = Aggregates::JobApplication.new(produced_events + [job_apply]).call
-    _(success.bool).must_equal(true)
+    @software_engineer.add_possible_action(
+      :work,
+      [Constructs::Vector.new(money_space, 123)],
+      []
+    )
 
     work = Events::Event.new(:schedule, @software_engineer, [], { as: :work,   from: '', till: ''})
 
     # ev5 and ev6 are before the work event time
-    produced_events = Aggregates::TimeProgress.new([work, ev5, ev6]).call
+    produced_events = Aggregates::TimeProgress.new([work, ev5, ev6]).call.events
     _(produced_events.empty?).must_equal true
 
-    ev7 = Events::GameTime.new(:tick, :game_time, [])
-    ev8 = Events::GameTime.new(:tick, :game_time, [])
-    ev9 = Events::GameTime.new(:tick, :game_time, [])
+    ev7 = Events::GameTime.new(:tick, :game_time, [], { speed_val: 1 })
+    ev8 = Events::GameTime.new(:tick, :game_time, [], { speed_val: 1 })
+    ev9 = Events::GameTime.new(:tick, :game_time, [], { speed_val: 1 })
 
-    produced_events = Aggregates::TimeProgress.new([work, ev7, ev8, ev9]).call
+    produced_events = Aggregates::TimeProgress.new([work, ev7, ev8, ev9]).call.events
     _(produced_events.length).must_equal 2
     _(produced_events.first.action).must_equal :work
     _(produced_events.first.target).must_equal @software_engineer
@@ -112,8 +92,8 @@ describe 'Game CLI mode - end to end' do
 
     _(result.to_h).must_equal(
       {
-        stats: { money: 60, cs_skill: 6, energy: -20 },
-        inventory: ""
+        stats: { money: "346" },
+        violations: []
       }
     )
   end
@@ -122,7 +102,7 @@ describe 'Game CLI mode - end to end' do
     base = {  energy: 0 }
     result = Aggregates::Stats.new(base, @events, []).call
     _(result.violations).must_equal(
-      [:game_ends, :game_ends]
+      ["Energy cannot go below 0"]
     )
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,16 +3,16 @@ module TestHelper
     s1 = Constructs::Space.new(:cs_skill)
     s2 = Constructs::Space.new(:energy)
 
-    s2.add_violation(->(v) { v < 0 }, :game_ends)
+    s2.add_violation(->(v) { v < 0 }, :energy_too_low, "Energy cannot go below 0")
 
     vec1 = Constructs::Vector.new(s1, 10)
     vec2 = Constructs::Vector.new(s2, -5)
 
-    cs_book = Static::Item.new(:cs_book, :permanent)
-    cookie  = Static::Item.new(:cookie, :consumable)
+    cs_book = Static::Allocatable.new(:cs_book, :permanent)
+    cookie  = Static::Allocatable.new(:cookie, :consumable)
 
-    cs_book.add_possible_action(:study, [vec1, vec2])
-    cookie.add_possible_action(:eat, [Constructs::Vector.new(s2, 14)])
+    cs_book.add_possible_action(:study, [vec1, vec2], [])
+    cookie.add_possible_action(:eat, [Constructs::Vector.new(s2, 14)], [])
 
     events = [
       Events::Event.new(:study, cs_book, [vec1, vec2]),
